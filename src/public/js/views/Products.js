7feefeb8ec08ce5c4667f3ef0b0734a25617e194
theluposki <theluposki@gmail.com>
2022-10-24 19:41:40 -0300

    products taable respon

--- src/public/js/views/Products.js
+++ src/public/js/views/Products.js
@@ -6,45 +6,73 @@ export const Products = {
   `
   <div class="products">
     <div class="prod_headers">
-      HEADERS PRODUCTS
+      <div class="p_h-info"></div>
+      <div class="p_h-action"></div>
+
+      <div class="p_h-search">
+        <input type="search" :placeholder="typesSearch"/>
+        <div class="types-searchs">
+          <label for="types1">
+            Nome
+            <input type="radio" id="types1" v-model="typesSearch" value="Pesquisar por nome"/>
+          </label>
+          <label for="types2">
+            Data
+            <input type="radio" id="types2" v-model="typesSearch" value="Pesquisar por data"/>
+          </label>
+          <label for="types3">
+            Preço
+            <input type="radio" id="types3" v-model="typesSearch" value="Pesquisar por preço"/>
+        </label>
+        </div>
+      </div>
+
     </div>
+    <div class="content-table">
+      <table class="table table-prod">
 
-    <table class="table_products">
+        <thead>
+          <tr>
+            <th><pre>#id</pre></th>
+            <th>Nome</th>
+            <th>Descrição</th>
+            <th>Preço de compra</th>
+            <th>Preço</th>
+            <th>Categoria</th>
+            <th>Marca</th>
+            <th>Unidade</th>
+            <th>Data de Criação</th>
+            <th></th>
+          </tr>
+        </thead>
 
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Descrição</th>
-          <th>Preço de compra</th>
-          <th>Preço</th>
-          <th>Categoria</th>
-          <th>Marca</th>
-          <th>Unidade</th>
-          <th>Data de Criação</th>
-          <th></th>
-        </tr>
-      </thead>
+        <tbody>
+          <tr v-for="prod in products" :key="prod.id">
+            <td>{{prod.id}}</td>
+            <td>{{prod.name}}</td>
+            <td>{{prod.desc}}</td>
+            <td>{{currency(prod.purchase_price)}}</td>
+            <td>{{currency(prod.price)}}</td>
+            <td>{{prod.category_id}}</td>
+            <td>{{prod.brand_id}}</td>
+            <td>{{prod.weight_id}}</td>
+            <td>{{dateFormat(prod.create_at)}}</td>
+            <td class="edit-buttons">
+              <button>
+                <i class="ai-edit"></i>
+              </button>
+            </td>
+          </tr>
+        </tbody>
 
-      <tbody>
-        <tr v-for="prod in products" :key="prod.id">
-          <td>{{prod.name}}</td>
-          <td>{{prod.desc}}</td>
-          <td>{{prod.purchase_price}}</td>
-          <td>{{prod.price}}</td>
-          <td>{{prod.category_id}}</td>
-          <td>{{prod.brand_id}}</td>
-          <td>{{prod.weight_id}}</td>
-          <td>{{prod.create_at}}</td>
-          <td class="edit-buttons"></td>
-        </tr>
-      </tbody>
-
-    </table>
+      </table>
+    </div>
   </div>
   `,
   data() {
     return {
       namePage: "Produtos",
+      typesSearch: "Pesquisar por nome",
       product: {
         name: null,
         desc: null,
@@ -67,6 +95,13 @@ export const Products = {
       const { data } = await axios.get("/products")
       console.log(data)
       this.products = data
+    },
+    currency(number, lang = "pt-br", currencyAcronyms = "BRL") {
+      const n = Number(number)
+      return new Intl.NumberFormat(lang, { style: 'currency', currency: currencyAcronyms }).format(n)
+    },
+    dateFormat(date, options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }) {
+      return new Intl.DateTimeFormat(options.lang, options).format(new Date(date))
     }
   }
 }
